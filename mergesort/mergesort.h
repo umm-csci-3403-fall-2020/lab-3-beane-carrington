8997e36c071832430e622533b3be0bdfc1686f0a
Robert-Beane <beane031@morris.umn.edu>
2020-09-22 11:03:29 -0500

    Add necessary functions so it can compile properly

--- mergesort/mergesort.h
+++ mergesort/mergesort.h
@@ -6,5 +6,8 @@
 #define UNIT_TESTING
 
 void mergesort(int size, int values[]);
+void mergesort_range(int *values, int startIndex, int endIndex);
+void merge_ranges(int *values, int startIndex, int midPoint, int endIndex);
+bool needs_sorting(int rangeSize);
 
 #endif
