fc07ba2a8993c58655934390e3207eeee8115ca5
Robert-Beane <beane031@morris.umn.edu>
2020-09-22 10:33:29 -0500

    implement mergeSortRange

--- mergesort/mergesort.c
+++ mergesort/mergesort.c
@@ -5,7 +5,16 @@
 #include "mergesort.h"
 
 void mergesort(int size, int values*) {
-  
-  
+  mergeSortRange(values, 0, size);
   return;
+}
+
+void mergeSortRange(int *values, int startIndex, int endIndex) {
+  int rangeSize = endIndex - startIndex;
+  if (needsSorting(rangeSize)){
+    int midPoint = (startIndex + endIndex) / 2;
+    mergeSortRange(values, startIndex, midPoint);
+    mergeSortRange(values, midPoint, endIndex);
+    mergeRanges(values, startIndex, midPoint, endIndex);
+  }
 }
