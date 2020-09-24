867f278356db5cadba3cd9d8ba9b0c6980488658
Robert-Beane <beane031@morris.umn.edu>
2020-09-24 10:17:58 -0500

    Add is_unique method for determining unique values.

--- array_merge/array_merge.c
+++ array_merge/array_merge.c
@@ -1,6 +1,22 @@
+#include "mergesort.h"
 #include "array_merge.h"
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+bool is_unique(int n, int index, int* temp){ // function for determing if a value is unique among the other arrays
+  bool unique = true; // starts off by setting it as true
+  for (int i = 0; i < index; i++){
+    if (n == temp[i]){
+      unique = false; // sets unique to false if it finds the same value
+      return unique;
+    }
+  }
+  return unique;
+}
+
 
 int* array_merge(int num_arrays, int* sizes, int** values) {
-  // This is obviously broken. It has the right type, though, eh?
+  
   return sizes;
 }
